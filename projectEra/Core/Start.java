57f0a6bd1320aa6244c9d6b04116e13d67f243f4
Caleb Kirby <milkproducktionskirby@gmail.com>
2014-02-18 21:02:05 -0500

    Update

--- projectEra/Core/Start.java
+++ projectEra/Core/Start.java
@@ -2,9 +2,16 @@ package projectEra.Core;
 
 import static org.lwjgl.opengl.GL11.*;
 
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
 import org.lwjgl.LWJGLException;
 import org.lwjgl.Sys;
 import org.lwjgl.opengl.Display;
+import org.lwjgl.opengl.DisplayMode;
 
 import projectEra.Game.StartMenu;
 import projectEra.Input.KeyboardListener;
@@ -12,19 +19,38 @@ import projectEra.Input.KeyboardListener;
 public class Start {
 	GameStates cgs = GameStates.startMenu;
 	
+	static boolean failed;
+	static boolean antiAlias = true;
+	
+    
 	//Initiate the whole program
 	public static void main(String args[]) {
 		lastFrame = getTime();
 		
-		initGraphics();
 		initDisplay();
+		initGraphics();
 		new Start().gameLoop();
 		cleanUp();
 	}
 	
 	//Initialize graphics
 	public static void initGraphics() {
+		glEnable(GL_TEXTURE_2D);
+		glShadeModel(GL_SMOOTH);
+		glDisable(GL_DEPTH_TEST);
+		glDisable(GL_LIGHTING);
 		
+		glClearDepth(1);
+		
+		glEnable(GL_BLEND);
+		glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+		
+		glViewport(0, 0, Display.getWidth(), Display.getHeight());
+		glMatrixMode(GL_MODELVIEW);
+		glMatrixMode(GL_PROJECTION);
+		glLoadIdentity();
+		glOrtho(0, Display.getWidth(), Display.getHeight(), 0, 1, -1);
+		glMatrixMode(GL_MODELVIEW);
 	}
 	
 	//Initiate inputs
@@ -32,43 +58,69 @@ public class Start {
 		switch (cgs) {
 		case startMenu:
 			new KeyboardListener().startMenu();
-			System.out.println("Doesn't Work");
 		default:
-			System.out.println("Works");
 			break;
 		}
 	}
 	
 	//Initiate display
 	public static void initDisplay() {
-		try {
-			Display.setTitle("Project Era Version 0.0.1 Indev");
-			Display.setInitialBackground(0.0f, 0.4f, 0.4f);
-			Display.setFullscreen(true);
-			Display.create();
-		} catch (LWJGLException e) {
-			e.printStackTrace();
+			DisplayMode[] modes = null;
+			try {
+				modes = Display.getAvailableDisplayModes();
+			} catch (LWJGLException e1) {
+				e1.printStackTrace();
+			}
+			
+			
+			DisplayMode current = null;
+			String s = null;
+			for (int i=0;i<modes.length;i++) {
+				current = modes[i];
+				System.out.println(current.getWidth() + "x" + current.getHeight() + "x" + current.getBitsPerPixel() + " " + current.getFrequency() + "Hz");
+				s = s + current.getWidth() + "x" + current.getHeight();
+			}
+			
+			if (!s.contains("1366x768")) {
+				resolutionError();
+				
+				failed = true;
+			}else {
+				try{
+					Display.setTitle("Project Era Version 0.0.1 Indev");
+					Display.setInitialBackground(0.0f, 0.4f, 0.4f);
+					Display.setFullscreen(true);
+					Display.create();
+			} catch (LWJGLException e) {
+				e.printStackTrace();
+			}
 		}
 	}
 	
 	//Game loop
 	public void gameLoop() {
+		if (!failed) {
 		while (!Display.isCloseRequested()) {
-			new Start().getInput();
-			
-			glClear(GL_COLOR_BUFFER_BIT);  
-			glMatrixMode(GL_PROJECTION);
-		    glLoadIdentity();
-		    glOrtho(0, Display.getWidth(), Display.getHeight(), 0, 1, -1);
-			switch(cgs) {
-			case startMenu:
-				new StartMenu();
-				break;
-			default:
-				break;
+				new Start().getInput();
+				
+				glClear(GL_COLOR_BUFFER_BIT);  
+				glMatrixMode(GL_PROJECTION);
+		    	glLoadIdentity();
+		    	glOrtho(0, Display.getWidth(), Display.getHeight(), 0, 1, -1);
+				switch(cgs) {
+				case startMenu:
+					new StartMenu();
+					break;
+				default:
+					break;
+				}
+				
+				Display.update();
 			}
-			
-			Display.update();
+		}else {
+			while (!Display.isCloseRequested()) {
+				Display.update();
+			}
 		}
 	}
 	
@@ -92,4 +144,19 @@ public class Start {
 	}
 	
 	//End movement delta handler
+	
+	public static void resolutionError() {
+		JFrame f = new JFrame("Failure");
+		JTextField t = new JTextField();
+		JPanel p = new JPanel();
+		t.setText("Failed to start, incompatible resolution");
+		t.setEditable(false);
+		f.setSize(new Dimension(250, 60));
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		f.setLocationRelativeTo(null);
+		f.setVisible(true);
+		f.setResizable(false);
+		p.add(t);
+		f.add(p);
+	}
 }
--- projectEra/Game/StartMenu.java
+++ projectEra/Game/StartMenu.java
@@ -2,7 +2,14 @@ package projectEra.Game;
 
 import static org.lwjgl.opengl.GL11.*;
 
+import java.awt.Font;
+
+import org.newdawn.slick.TrueTypeFont;
+
+
 public class StartMenu {
+	TrueTypeFont font;
+	
 	public StartMenu() {
 		glBegin(GL_QUAD_STRIP);
 		{
@@ -12,5 +19,13 @@ public class StartMenu {
 			glVertex2f(200, 200);
 		}
 		glEnd();
+		Font awtFont = new Font("Times New Roman", Font.BOLD, 24);
+		font = new TrueTypeFont(awtFont, true);
+		
+		
+		for (int i = 0; i < 10; i++) {
+			glClear(GL_COLOR_BUFFER_BIT);
+			font.drawString(100, 100, Integer.toString(i));
+		}
 	}
 }
